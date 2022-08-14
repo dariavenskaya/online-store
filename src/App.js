28cd125b95b419dc5a791b1f66f90e5fa2727fe4
Дарья <daria@MacBook-Pro-macbook.local>
2022-08-14 17:10:23 +0300

    feat: add cart page and layout

--- src/App.js
+++ src/App.js
@@ -4,25 +4,21 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header";
-import Home from './pages/Home'
-import Cart from './pages/Cart'
-import NotFound from './pages/NotFound'
-
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import NotFound from "./pages/NotFound";
 
 function App() {
-
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-        <div className="container">
-          <div className="center">
+        <div className="center">
           <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="cart" element={<Cart />} />
-          <Route path="*" element={<NotFound />} />
+            <Route path="/" element={<Home />} />
+            <Route path="cart" element={<Cart />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
-          </div>
         </div>
       </div>
     </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,35 +5,35 @@ import Sort from "../components/Sort";
 import Card from "../components/Card";
 import Skeleton from "../components/Card/Skeleton";
 
-
 function Home() {
-
   const [items, setItems] = React.useState([]);
-  const [isLoading, setIsLoading] = React.useState(true);  
+  const [isLoading, setIsLoading] = React.useState(true);
 
-  React.useEffect( () =>{
-    fetch('https://62f7c196ab9f1f8e8902d90e.mockapi.io/items')
-    .then(response => response.json())
-    .then(json => {
-        setItems(json)
-        setIsLoading(false)
-    })
-  }, [] )
-  
-  return(
+  React.useEffect(() => {
+    fetch("https://62f7c196ab9f1f8e8902d90e.mockapi.io/items")
+      .then((response) => response.json())
+      .then((json) => {
+        setItems(json);
+        setIsLoading(false);
+      });
+    window.scrollTo(0, 0);
+  }, []);
+
+  return (
     <>
-      <div className="content__top">
-        <Categories />
-        <Sort />
-      </div>
-      <div className="content__items">
-        {isLoading
-        ? [...new Array(5)].map((_, i) => <Skeleton key={i} />) 
-        : items.map((item) => <Card key={item.id} {...item} />)
-        }
+      <div className="container">
+        <div className="content__top">
+          <Categories />
+          <Sort />
+        </div>
+        <div className="content__items">
+          {isLoading
+            ? [...new Array(5)].map((_, i) => <Skeleton key={i} />)
+            : items.map((item) => <Card key={item.id} {...item} />)}
+        </div>
       </div>
     </>
-  )
+  );
 }
 
 export default Home;
